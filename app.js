8aef7e120b2fd738dd672196418b30cb766214d5
MoWael11 <midonit06@gmail.com>
2023-06-01 19:19:11 +0200

    fd

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  const ipAddress = req.socket.remoteAddress;
-  res.send(ipAddress);
+  const ip = 
+  req.headers['cf-connecting-ip'] ||
+  req.headers['x-real-ip'] ||
+  req.headers['x-forwarded-for'] ||
+  req.socket.remoteAddress || ''
+  res.json(ip)
 })
 
 app.listen(3000)
